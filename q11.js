da0ac213fbd133b4fd8291f0c86f92dd1e2425da
Pravali-2-7 <pravalikapotta000@gmail.com>
2025-11-25 17:49:44 +0530

    L0 - Transforming and Filtering with map() and forEach()

---
+++ q11.js
@@ -0,0 +1,23 @@
+const products = [
+  { name: "Laptop", price: 1000 },
+  { name: "Mouse", price: 20 },
+  { name: "Keyboard", price: 60 },
+  { name: "USB Cable", price: 15 }
+];
+
+function processProducts(products) {
+  const productNames = products.map(product => product.name);
+
+  products.forEach(product => {
+    if (product.price > 50) {
+      console.log(`${product.name} is above $50`);
+    } else {
+      console.log(`${product.name} is below $50`);
+    }
+  });
+
+  return productNames;
+}
+
+const names = processProducts(products);
+console.log(names);

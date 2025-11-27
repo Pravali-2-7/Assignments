5822fbc33727f6e7392369326ef8ec3e8236348d
Pravali-2-7 <pravalikapotta000@gmail.com>
2025-11-27 16:57:38 +0530

    L1 - Simulating a Loading Screen with setInterval

---
+++ Que13.js
@@ -0,0 +1,7 @@
+const loading = setInterval(function () {
+  console.log("Loading...");
+}, 1000);
+setTimeout(function () {
+  clearInterval(loading);
+  console.log("Loaded successfully!");
+}, 5000);
